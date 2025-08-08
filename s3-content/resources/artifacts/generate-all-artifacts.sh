0241c88b5cf068c94c55ba3c57dab92e268b9088
Cindy Zhao <46728318+cinlo@users.noreply.github.com>
2025-08-08 11:09:53 -0700

    Delete s3-content directory

--- s3-content/resources/artifacts/generate-all-artifacts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# generate-all-artifacts.sh
-
-cd ../helm-chart-injector && ./generate-helm-lambda-zip.sh
-cd ../fsx-for-lustre && ./generate-fsx-lambda-zip.sh
-cd ../hyperpod-cluster-creator && ./generate-hp-lambda-zip.sh
--- s3-content/resources/fsx-for-lustre/generate-fsx-lambda-zip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# generate-fsx-lambda-zip.sh
-
-# Build the Lambda layer using Docker
-./generate-fsx-lambda-layer.sh
-
-# Package the Lambda function with dependencies
-./generate-fsx-lambda-func.sh
-
